79d4cfeeedb37a3e6c4a1898dc92ea527775c360
luschettini <luizaschettinii@gmail.com>
2024-03-12 09:03:51 -0300

    commit um

---
+++ ex8.js
@@ -0,0 +1,17 @@
+//Lembrando que o JS é case senstive
+//A diferente de a
+
+//Iniciar o valor de A
+let a = 10;
+
+//iniciar o valor de B
+let b = 13
+
+//realiza soma
+let soma =a +b;
+
+//Exibo resultado
+console.log (soma);
+
+
+
