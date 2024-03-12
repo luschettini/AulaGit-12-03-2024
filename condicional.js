0186470f7cd07264225ce0e6a33bf232fe3a33c6
luschettini <luizaschettinii@gmail.com>
2024-03-12 08:49:22 -0300

    Não houve mudança no código

---
+++ condicional.js
@@ -0,0 +1,13 @@
+//Iniciar numero
+let numero = 0;
+
+//Fazer a condicao: SE ou IF 
+if(numero > 0){
+    //exiba numero positivo 
+    console.log("Esse numero é positivo");
+}else if(numero < 0){
+    //Exiba numero negativo
+    console.log("Esse numero é negativo")
+}else{
+    console.log("O numero é 0");
+}
